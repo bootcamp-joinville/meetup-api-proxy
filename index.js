24645898912d7158d56be3098a681fcd57d1b62d
Diogo Trentini <diogotrentini@gmail.com>
2018-07-23 20:02:39 -0300

    Add events endpoint

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const path = require('path')
+const request = require('request')
 const PORT = process.env.PORT || 5000
 
-express()
+const app = express()
+
+app
+  .get('/events', (_, res) => {
+    const URL = 'https://api.meetup.com/find/upcoming_events?key=TOKEN'
+
+    request(URL, { json: true }, (err, _, body) => {
+      err ? res.json({ error: err }) : res.json(body)
+    })
+  })
+
+app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
